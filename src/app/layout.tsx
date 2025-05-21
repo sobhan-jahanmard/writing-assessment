ede8d54104220e3cfca26b3445c1bdbd95522d02
Sobhan <s.jahanmard@gmail.com>
2025-05-21 21:03:58 +0330

    feat: add vazir font and fix tailwind colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
+    <html lang="fa" dir="rtl">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
