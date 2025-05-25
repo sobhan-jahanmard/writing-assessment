27612bc3100d47807074035251f717af43b09e99
Sobhan <s.jahanmard@gmail.com>
2025-05-25 13:48:52 +0330

    feat: add types

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,8 @@
+import { Database } from "@/database.types";
 import { createBrowserClient as CBC } from "@supabase/ssr";
 
 export function createBrowserClient() {
-  return CBC(
+  return CBC<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
