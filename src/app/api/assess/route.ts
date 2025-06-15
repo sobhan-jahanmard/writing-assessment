f613dafaaebd1f319145ff9c2f14251157dfad7d
Sobhan <s.jahanmard@gmail.com>
2025-06-15 22:59:44 +0330

    feat: add endpoint for doing job

--- src/app/api/assess/route.ts
+++ src/app/api/assess/route.ts
@@ -2,6 +2,6 @@ import { processNextJob } from "@/src/lib/services/queue.service";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  processNextJob();
+  await processNextJob();
   return NextResponse.json({ message: "Job processed" });
 }
