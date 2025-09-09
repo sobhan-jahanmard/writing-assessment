073513dbd9c10432f1af7eac4f2846c07291db97
Sobhan <s.jahanmard@gmail.com>
2025-09-09 19:10:40 +0330

    fix: make api/asess public

--- src/lib/is-public-pages.ts
+++ src/lib/is-public-pages.ts
@@ -1,11 +1,10 @@
 export function isPublicPage(pathname: string) {
-  if (pathname === "/") {
-    return true;
-  } else if (pathname.startsWith("/auth/login")) {
-    return true;
-  } else if (pathname.startsWith("/auth/signup")) {
-    return true;
-  } else if (pathname.startsWith("/error")) {
+  if (
+    pathname === "/" ||
+    ["/auth/login", "/auth/signup", "error", "/api/assess"]?.some((item) =>
+      pathname.startsWith(item)
+    )
+  ) {
     return true;
   } else {
     return false;
